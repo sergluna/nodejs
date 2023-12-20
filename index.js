0af526d4e434cce1f302ace9ac8c0e11e141486c
Moon Man <111019074+sergluna@users.noreply.github.com>
2023-12-19 21:54:36 -0600

    nodejs and npm

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+// // blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+// console.log(textIn);
+
+// const textOut = `A little information about avocado: ${textIn}. \nCreated by Sergio on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// console.log("File Written!");
+
+// nonblocking, asynchronous
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written ");
+      });
+    });
+  });
+});
+
+console.log("Will read file");
